9f87068b59784b08166e7867f057a017c1c6eed9
Christophe KUNZLI <Christophe.KUNZLI@cpnv.ch>
2023-03-01 16:55:02 +0100

    Continue implementing main Form

--- Gestionnaire-TPI/MainForm.Designer.cs
+++ Gestionnaire-TPI/MainForm.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.dgvListTPI = new System.Windows.Forms.DataGridView();
+            this.cmdAdd = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvListTPI)).BeginInit();
             this.SuspendLayout();
             // 
@@ -37,18 +38,31 @@
             this.dgvListTPI.AllowUserToAddRows = false;
             this.dgvListTPI.AllowUserToDeleteRows = false;
             this.dgvListTPI.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvListTPI.Location = new System.Drawing.Point(12, 12);
+            this.dgvListTPI.Location = new System.Drawing.Point(12, 36);
             this.dgvListTPI.Name = "dgvListTPI";
             this.dgvListTPI.ReadOnly = true;
             this.dgvListTPI.RowTemplate.Height = 25;
-            this.dgvListTPI.Size = new System.Drawing.Size(1161, 426);
+            this.dgvListTPI.Size = new System.Drawing.Size(1137, 402);
             this.dgvListTPI.TabIndex = 0;
+            // 
+            // cmdAdd
+            // 
+            this.cmdAdd.Enabled = false;
+            this.cmdAdd.Location = new System.Drawing.Point(12, 7);
+            this.cmdAdd.Name = "cmdAdd";
+            this.cmdAdd.Size = new System.Drawing.Size(135, 23);
+            this.cmdAdd.TabIndex = 1;
+            this.cmdAdd.Text = "Nouveau TPI";
+            this.cmdAdd.UseVisualStyleBackColor = true;
+            this.cmdAdd.Visible = false;
+            this.cmdAdd.Click += new System.EventHandler(this.cmdAdd_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1185, 450);
+            this.ClientSize = new System.Drawing.Size(1161, 450);
+            this.Controls.Add(this.cmdAdd);
             this.Controls.Add(this.dgvListTPI);
             this.Name = "MainForm";
             this.Text = "Main";
@@ -60,5 +74,6 @@
         #endregion
 
         private DataGridView dgvListTPI;
+        private Button cmdAdd;
     }
 }
--- Gestionnaire-TPI/MainForm.cs
+++ Gestionnaire-TPI/MainForm.cs
@@ -21,33 +21,69 @@ namespace Gestionnaire_TPI
             this.user = user;
             
             initDGV();
+
         }
 
         private void initDGV()
         {
-            dgvListTPI.ColumnCount = 6;
+            //Add all columns
+            dgvListTPI.ColumnCount = 4;
 
             dgvListTPI.Columns[0].Name = "Titre";
             dgvListTPI.Columns[0].Width = 500;
             dgvListTPI.Columns[1].Name = "Date";
-            dgvListTPI.Columns[2].Name = "Remarques";
-            dgvListTPI.Columns[3].Name = "Durée";
-            dgvListTPI.Columns[4].Name = "Candidat";
-            dgvListTPI.Columns[5].Name = "Chef de projet";
+            dgvListTPI.Columns[2].Name = "Candidat";
+            dgvListTPI.Columns[3].Name = "Chef de projet";
 
-
+            //Prevent user from manipulating the data
             dgvListTPI.ReadOnly = true;
             dgvListTPI.AllowUserToAddRows = false;
             dgvListTPI.AllowUserToDeleteRows = false;
             dgvListTPI.AllowUserToOrderColumns = false;
-            //dgvListTPI.AllowUserToResizeColumns = false;
+
+            dgvListTPI.AllowUserToResizeColumns = true;
             dgvListTPI.AllowUserToResizeRows = false;
             dgvListTPI.AllowDrop = false;
 
+
+            if (user.IsAdmin)
+            {
+                cmdAdd.Visible = true;
+                cmdAdd.Enabled = true;
+
+                DataGridViewButtonColumn button = new DataGridViewButtonColumn();
+                button.Text = "Supprimmer";
+                button.Name = "cmdDelete";
+
+                dgvListTPI.Columns.Add(button);
+
+                dgvListTPI.CellContentClick += new DataGridViewCellEventHandler(dgvListTPI_CellContentClick);
+            }
+
             displayList();
 
+
         }
-        
+
+        /// <summary>
+        /// Event handler for the delete button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void dgvListTPI_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var senderGrid = (DataGridView)sender;
+
+            if (senderGrid.Columns[e.ColumnIndex] is DataGridViewButtonColumn && e.RowIndex >= 0)
+            {
+                MessageBox.Show(dgvListTPI[0, e.RowIndex].Value.ToString());
+            }
+        }
+
+
+        /// <summary>
+        /// Insert all data to the DataGridView
+        /// </summary>
         private void displayList()
         {
             dgvListTPI.Rows.Clear();
@@ -69,6 +105,19 @@ namespace Gestionnaire_TPI
                 MessageBox.Show(ex.Message);
             }
         }
-        
+
+
+        private void cmdAdd_Click(object sender, EventArgs e)
+        {
+            Form addTPI = new Form();
+
+
+            addTPI.FormClosing += delegate {
+                this.Show();
+            };
+
+            addTPI.Show();
+            this.Hide();
+        }
     }
 }
