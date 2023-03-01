194f82fb09a84a2a5ce79ce1c8f6469663c5af28
Christophe KUNZLI <Christophe.KUNZLI@cpnv.ch>
2023-03-01 12:05:36 +0100

    Add TPI class

---
+++ Gestionnaire-TPI/TPI.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestionnaire_TPI
+{
+    public class TPI
+    {
+        private string title;
+        private string year;
+        private string remarks;
+        private string duration;
+
+        public string Title { get { return title; } }
+        public string Year { get { return year; } }
+        public string Remarks { get { return remarks; } }
+        public string Duration { get { return duration; } }
+        
+        
+        public TPI(string title) 
+        { 
+            this.title = title;
+        }
+
+        public TPI(string title, string year, string remarks, string duration)
+        {
+            this.title = title;
+            this.year = year;
+            this.remarks = remarks;
+            this.duration = duration;
+        }
+
+        public string[] displayTPI()
+        {
+            return new string[] { title, year, remarks, duration };
+        }
+    }
+}
