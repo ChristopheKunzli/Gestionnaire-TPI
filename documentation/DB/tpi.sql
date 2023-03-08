f7ba59e73670fa299d302f31b7392c510de06af9
Christophe KUNZLI <Christophe.KUNZLI@cpnv.ch>
2023-03-08 16:42:15 +0100

    Organize files in Documentation/DB directory

--- documentation/DB/tpi.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO tpi (title,year,Candidates_id,Collaborators_id)
-VALUES
-('Consultation et archivage des sujets donnés lors des PRE-TPI et TPI à l\'aide d\'une application c#','2023-01-31', 106,2),
-('Mise en service de GPI avec liaison Active Directory','2023-01-31',11,41)
-;
